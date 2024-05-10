34623c161d5d2545cd77028decec2d40b0fe0061
jakkoz0803 <115089022+jakkoz0803@users.noreply.github.com>
2024-05-10 18:47:57 +0200

    Add files via upload

---
+++ lab9/lab9_4.pl
@@ -0,0 +1,14 @@
+% podwojenie(+L1,?L2).
+% spe³niony, gdy elementy listy L2 podwojniami
+% elementow listy L1, np. L1=[a,b], L2=[a,a,b,b]
+% ------------------------------------------------
+
+% warunek koñcz¹cy rekurencjê: podwojenie listy pustej
+% jest list¹ pust¹.
+
+podwojenie([],[]).
+
+% rekurencja:
+
+podwojenie([H1|T1],[H1,H1|T2]):-
+    podwojenie(T1,T2).
